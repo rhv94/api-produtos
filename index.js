aec26c6cbc16f6b423ffd225727c68d8f065d168
rhv94 <rebeca_hv@hotmail.com>
2025-06-10 11:08:32 -0300

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 6579;
+const port = process.env.PORT || 6579;
 require('./src/models')
 const produtosRoutes = require('./src/routes/produtos')
 const usersRoutes = require('./src/routes/users')
